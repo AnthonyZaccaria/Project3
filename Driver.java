dda8f41d98aa2a09a655ac9e55069c73c5934e6c
AnthonyZaccaria <a.m.zaccaria@email.msmary.edu>
2024-04-25 16:52:09 -0400

    ValueFinished

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
+
 public class Driver {
     public static void main(String[] args) {
-        Node n1 = new Node(6);
+        Node n1 = new Node(6,0);
     }
 
     /**
@@ -8,19 +9,36 @@ public class Driver {
      * @param n: node
      * @returns int that is the value
      */
-    public static int f(Node n){
-        int val = 0;
+    public static double f(Node n){
+        double val = 0;//value of node (will be variance of nums)
         int len = (n.getSize()/2);//length of sums array
         int[] sums = new int[len];
-        for (int i = 1;i<n.getNums().length;i+=2){
-            sums[i%len] = n.getNums()[0]+n.getNums()[i]+n.getNums()[i+1];
+        int sum = 0;//sum of sums
+        double mean = 0;//mean of sums
+        int[] numsCopy = n.getNums();//not changing it at all so we can do this
+        //gets sums of diagnols
+        for (int i = 1;i<numsCopy.length;i+=2){
+            sums[i%len] = numsCopy[0]+numsCopy[i]+numsCopy[i+1];
+            sum+=sums[i%len];
         }
+        mean = sum*1.0/len*1.0; 
 
-
+        for (int i=0;i<sums.length;i++){
+            double diff = 0.0;
+            diff = sums[i]-mean;
+            if (diff<0) diff *= -1.0;
+            val += diff;            
+        }
+        val /= len*1.0;
         return val;
     }
 
-    public static int[] getChildren(Node n){
+    /**
+     * gets children of a node
+     * @param n
+     * @returns a list of nodes that is the children
+     */
+    public static Node[] getChildren(Node n){
         return null;
     }
 
@@ -30,8 +48,6 @@ public class Driver {
      * @returns solution node (or no solution)
      */
     public static Node hill_climb(Node start){
-        
-        
         
         return null;
     }
