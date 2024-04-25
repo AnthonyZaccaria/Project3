79aed2c9b07cf9d2d6b1a4454801bfa6afb8146f
AnthonyZaccaria <a.m.zaccaria@email.msmary.edu>
2024-04-25 16:10:18 -0400

    update4.1PM

--- Driver.java
+++ Driver.java
@@ -1,7 +1,10 @@
 public class Driver {
-    
     public static void main(String[] args) {
-        
+        Node n1 = new Node(6);
+    }
+
+    public static int[] getChildren(Node n){
+        return null;
     }
 
 }
--- Node.java
+++ Node.java
@@ -1,3 +1,23 @@
 public class Node {
-    
+    private int size;//either 6, 10, or 14
+    private int[] nums;//going to be length size+1
+    private int loc; //location of the biggest number
+    private int biggest;
+    //need to represen flower into computer
+    // center number is 0
+    // 1 is opposite of 2, 3 is opposite of 4, etc...
+    // So 1+0+2 is a diagnol, 3+0+4 is a diagnol, etc...
+    public Node(int size){
+        nums = new int[size+1];
+        biggest = size+1;
+        nums[loc] = biggest;
+    }
+
+
+
+
+
+
+
+
 }
